b50ae88f40b9983b476bf843cc1fad0a761a3231
Jihad Ballout <148473573+yeezySznApproaching@users.noreply.github.com>
2025-01-21 16:04:05 +0100

    added css using StyleSheet

--- ReactNativeNotesApp/app/(tabs)/test.tsx
+++ ReactNativeNotesApp/app/(tabs)/test.tsx
@@ -1,10 +1,26 @@
-import { View, Text} from 'react-native'
+import { View, Text, StyleSheet} from 'react-native'
 import React from 'react'
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        width: "100%",
+        height: "100%",
+        alignItems: "center",
+    },
+    title: {
+        color: "white",
+        fontSize: 24,
+    }
+})
+
+
 
 const test = () => {
     return (
-        <View>
-            <Text>Test</Text>
+        <View style= {styles.container}>
+            <Text style= {styles.title}>Test</Text>
         </View>
     )
 }
